10c89f154163277567453f4e93c462bb28190b8d
lucasvilaofinaszl <lucas.alves.portela@escola.pr.gov.br>
2025-06-09 07:42:09 -0300

    .

--- man.js
+++ man.js
@@ -1,2 +1,25 @@
 const mario = document.querySelector(".mario")
 const pipe = document.querySelector(".pipe")
+
+cont jump = () => {
+    mario.classList.add("jump");
+
+    setTimeout(() => {
+        mario.classList.remove("jump");
+    }
+    ,500);
+}
+
+const loop = setInterval(() => {
+const pipePosition = pipe.offsetLeft;
+const marioPosition = +window.getComputedStyle(mario).bottom.replace("px","");
+
+  if(pipePosition<= 120 && pipePosition > 0 &&marioPosition <80) {
+    pipe.computedStyle.animation= "none";
+    pipe.style.left =`${pipePosition}px`;
+
+    mario.style.animation ="none";
+    mario.style.left = `${marioPosition}px`;
+  }
+})
+document.addEventListener("keydown",jump);
